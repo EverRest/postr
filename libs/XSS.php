2a362f18f38fcade001c4eed259612a3280fe981
gmg-907 <medynskyypavlo@gmail.com>
2017-01-11 19:38:59 +0200

    xss security v.2 commit

--- libs/XSS.php
+++ libs/XSS.php
@@ -4,7 +4,6 @@ class XSS
     public static function xss_cleaner($request)
     {
         $filter = array("<", ">","="," (",")",";","/");
-
         foreach ($request as $num => $xss) {
             $request[$num] = str_replace($filter, "|", $xss);
         }
