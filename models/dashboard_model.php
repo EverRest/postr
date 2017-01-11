5137b022a96c682e3799056eadf3fb4c70447142
gmg-907 <medynskyypavlo@gmail.com>
2017-01-11 19:43:39 +0200

    xss security v.3 commit

--- models/dashboard_model.php
+++ models/dashboard_model.php
@@ -5,13 +5,14 @@ class Dashboard_Model extends Model {
     }
 
     function xhrInsert() {
+        $_REQUEST = XSS::xss_cleaner($_REQUEST);
         if (isset($_POST['text']) AND !empty($_POST['text'])) {
-            $name = XSS::xss_cleaner($_POST['name']);
-            $email = XSS::xss_cleaner($_POST['email']);
-            $site = XSS::xss_cleaner($_POST['site']);
-            $text = XSS::xss_cleaner($_POST['text']);
-            $ip = XSS::xss_cleaner($_POST['ip']);
-            $client = XSS::xss_cleaner($_POST['client']);
+            $name = $_POST['name'];
+            $email = $_POST['email'];
+            $site = $_POST['site'];
+            $text = $_POST['text'];
+            $ip = $_POST['ip'];
+            $client = $_POST['client'];
             $sth = $this->db->prepare('INSERT INTO dashboard(name, email, site, text, ip, client) VALUES(:name, :email, :site, :text, :ip, :client)');
             $sth->execute(array(':name' => $name, ':email' => $email, ':site' => $site, ':text' => $text, ':ip' => $ip, ':client' => $client));
             $data = array('text' => $text, 'id' => $this->db->lastInsertId());
@@ -28,19 +29,21 @@ class Dashboard_Model extends Model {
     }
 
     public function xhrDeleteListing() {
-        $id = XSS::xss_cleaner($_POST['id']);
+        $_REQUEST = XSS::xss_cleaner($_REQUEST);
+        $id = $_POST['id'];
         $sth = $this->db->prepare('DELETE FROM dashboard WHERE id = "'.$id.'"');
         $sth->execute();
     }
 
     public function xhrEditListing()
     {
+        $_REQUEST = XSS::xss_cleaner($_REQUEST);
         if (isset($_POST['text']) AND !empty($_POST['text'])) {
-            $id = XSS::xss_cleaner($_POST['id']);
-            $name = XSS::xss_cleaner($_POST['name']);
-            $email = XSS::xss_cleaner($_POST['email']);
-            $site = XSS::xss_cleaner($_POST['site']);
-            $text = XSS::xss_cleaner($_POST['text']);
+            $id = $_POST['id'];
+            $name = $_POST['name'];
+            $email = $_POST['email'];
+            $site = $_POST['site'];
+            $text = $_POST['text'];
 
             $sth = $this->db->prepare('UPDATE dashboard SET name = :name, email = :email, site = :site, text = :text WHERE id = :id');
             $sth->execute(array(':name' => $name, ':email' => $email, ':site' => $site, ':text' => $text, ':id' => $id));
@@ -49,7 +52,8 @@ class Dashboard_Model extends Model {
 
     public function xhrGetNoteById()
     {
-        $id = XSS::xss_cleaner($_POST['id']);
+        $_REQUEST = XSS::xss_cleaner($_REQUEST);
+        $id = $_POST['id'];
         $sth = $this->db->prepare('SELECT name, email, site, text, ip, client FROM dashboard WHERE id = :id');
         $sth->setFetchMode(PDO::FETCH_ASSOC);
         $sth->execute(array('id' => $id));
